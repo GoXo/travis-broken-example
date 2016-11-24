a9b82bf6dd218b8ef0aba27d93eb4b33299b67d2
GoXo <aguirre.soto.alejandro@gmail.com>
2016-11-24 14:01:33 -0500

    Second attempt

--- Test.php
+++ Test.php
@@ -1,1 +1,10 @@
-2=1+1
+<?php
+	echo "TESTING..";
+	class Test extends PHPUnit_Framework_TestCase{
+    
+    public function testCalculate()
+    	{
+        	$this->assertEquals(2, 1 + 1);
+    	}
+	}
+?>
